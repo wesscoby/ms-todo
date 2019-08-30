f9c108053db7726e6ccaca131226c9db711c9a18
WessCoby <cobygiven@gmail.com>
2019-08-30 12:09:51 +0000

    Added JSON data [menu-items.json]

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
-import { MDBRow, MDBCol } from 'mdbreact';
+import { MDBRow, MDBCol, MDBIcon } from 'mdbreact';
+import TaskList from './TaskList';
 
 class Main extends Component {
     render() {
         return (
-            <MDBCol role="main" md="8" lg="9" className="ms-sm-auto px-4">
+            <MDBCol role="main" md="8" lg="9" className="ms-sm-auto p-4">
                 <MDBRow className="d-flex flex-column">
+                    <MDBCol>
+                        <MDBRow className="d-flex flex-row justify-content-between">
+                            <MDBCol className="d-flex flex-column mb-2">
+                                <h4>
+                                    My Day
+                                    <span id="elipsis" className="mx-3">...</span>
+                                </h4>
+                                <p id="date">Thursday, August 29</p>
+                            </MDBCol>
+                            <MDBCol className="d-flex flex-row justify-content-end">
+                                <p className="mr-2"><MDBIcon far icon="lightbulb" /> Today</p>
+                                <p className="ml-1"><MDBIcon icon="sort" /> Sort</p>
+                            </MDBCol>
+                        </MDBRow>
+                    </MDBCol>
+                    <MDBCol className="mb-3">
+                    <MDBIcon icon="plus" /> Add Task
+                    </MDBCol>
+                    <TaskList />
                 </MDBRow>
             </MDBCol>
         )
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,22 +1,30 @@
 import React, { Component, Fragment } from 'react';
-import { MDBCol, MDBNav, MDBNavItem, MDBIcon } from 'mdbreact';
+import { MDBCol, MDBNav, MDBIcon } from 'mdbreact';
+import MenuItem from './MenuItem';
+import MenuItems from '../data/menu-items.json';
 
 
 class Sidebar extends Component {
   state = {
-    
+    items: MenuItems.items
   }
 
   render() {
       return (
-          <MDBCol md="3" className="d-none d-md-block sidebar" style={{ height: "100vh" }}>
+          <MDBCol md="3" className="d-none d-md-block sidebar p-3">
             <Fragment className="sidebar-sticky">
               <MDBNav className="flex-column">
-                <MDBNavItem>
-                  <Fragment className="nav-link active">
-                    <MDBIcon icon="home" /> My Day
-                  </Fragment>
-                </MDBNavItem>
+                <a class="navbar-brand col-sm-3 col-md-2 mr-0 mb-3" href="#"><MDBIcon icon="bars" /></a>
+                {this.state.items.map(
+                  ({id, name, count, icon, active}) => 
+                    <MenuItem 
+                      key={id} 
+                      name={name}
+                      icon={icon}
+                      count={(count > 0) ? count : ''}
+                      active={(active) ? 'active' : ''}
+                    />
+                )}
               </MDBNav>
             </Fragment>
           </MDBCol>
