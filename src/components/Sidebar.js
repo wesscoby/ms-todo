2da57f7e93203f8fc6acff0dc5b66b363ee5fda4
WessCoby <cobygiven@gmail.com>
2019-08-31 09:28:12 +0000

    Minor style tweaks. Moved list state to main component (App) instead of Sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,29 +1,26 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { MDBCol, MDBNav, MDBIcon } from 'mdbreact';
 import { NavLink } from 'react-router-dom';
 import MenuItem from './MenuItem';
-import MenuItems from '../data/menu-items.json';
+// import MenuItems from '../data/menu-items.json';
 
 
 class Sidebar extends Component {
-  state = {
-    items: MenuItems.items
-  }
 
   render() {
       return (
           <MDBCol md="3" className="d-none d-md-block sidebar p-3">
-            <Fragment className="sidebar-sticky">
+            <div className="sidebar-sticky">
               <MDBNav className="flex-column">
                 <NavLink to='/' 
-                  class="navbar-brand col-sm-3 col-md-2 mr-0 mb-3 hamburger text-blue" 
+                  className="navbar-brand col-sm-3 col-md-2 mr-0 mb-3 hamburger text-blue" 
                 >
                   <MDBIcon icon="bars" size="1x"/>
                 </NavLink>
                 {/* List of  menu items:
                     Pass each object in state.items as prop to MenuItem component 
                 */}
-                {this.state.items.map(
+                {this.props.menuItems.map(
                   ({id, name, count, icon, active, color}) => 
                     <MenuItem className="mb-2"
                       key={id} 
@@ -32,10 +29,11 @@ class Sidebar extends Component {
                       iconColor={color}
                       count={(count > 0) ? count : ''}
                       active={(active) ? 'active' : ''}
+                      id={id}
                     />
                 )}
               </MDBNav>
-            </Fragment>
+            </div>
           </MDBCol>
       )
   }
