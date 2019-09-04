400c0164893a03fd7a3fe2f1dcc4f340a7a291e7
WessCoby <cobygiven@gmail.com>
2019-09-04 14:37:55 +0000

    Sidebar style tweaks including scrollbar style

--- src/components/ui/MenuItem.js
+++ src/components/ui/MenuItem.js
@@ -20,17 +20,28 @@ const MenuItem = ({ tasks, icon, id, color, name, activeList, changeActive, remo
         }
     }
 
+    const taskCountElement = count => (<p className="mr-2">{count}</p>)
+
     return (
-        <MDBNavItem  className={`d-flex flex-row justify-content-between ${activeList}`}>
+        <MDBNavItem  className={`d-flex flex-row justify-content-between mx-1 ${activeList}`}>
             <button onClick={makeActive} onDoubleClick={remove} type="submit" value={id} className="btn btn-link p-2">
                 <MDBIcon icon={icon} className={`${color} mr-2`}/>{name}
             </button>
             <Fragment>
-                {
-                    (id !== "5") ?
-                    tasks.filter(task => task.listId === id).length :
-                    tasks.length
-                }
+                {/*
+                    Filter number of tasks according to Active List
+                    But return total number of tasks if active list is Tasks
+                */}
+                
+                {((fn) => {
+                    if(id !== "5") {
+                        let filteredTasksLength = tasks.filter(task => task.listId === id).length
+                        return (filteredTasksLength > 0) ? 
+                            fn(filteredTasksLength) : fn('');
+                    } else {
+                        return fn(tasks.length) 
+                    }
+                })(taskCountElement)}
             </Fragment>
         </MDBNavItem>
     )
--- src/components/ui/Sidebar.js
+++ src/components/ui/Sidebar.js
@@ -4,29 +4,44 @@ import MenuItem from '../ui/MenuItem';
 
 
 const Sidebar = ({ lists, tasks, activeList, addList, removeList, changeActive }) => (
-    <MDBCol md="3" className="d-none d-md-block sidebar p-3">
-      <div className="sidebar-sticky">
-        <MDBNav className="d-flex flex-column list">
-          <button className="navbar-brand col-sm-3 col-md-2 mr-0 mb-3 hamburger">
-            <MDBIcon icon="bars" size="1x" className="text-blue" />
-          </button>
-          {lists.map(list => (
-            <MenuItem 
-              key={list.id} 
-              {...list}
-              activeList={(list.id === activeList) ? 'active' : ''}
-              changeActive={changeActive}
-              removeList={removeList}
-              tasks={tasks}
-            />
-          ))}
-        </MDBNav>
-      </div>
-        <div className="new-list d-flex flex-row justify-content-between">
-          <button className="btn btn-link p-2"><MDBIcon icon="plus" className="blue-text" /> New List</button>
-          <button className="btn btn-link p-2"><MDBIcon far icon="object-group" className="blue-text"/></button>
+    // Sidebar
+    <MDBCol md="3" className="d-none d-md-block sidebar">
+
+        <div className="sidebar-sticky">
+            <MDBNav className="d-flex flex-column">
+            
+                {/* Hamburger Button */}
+                <div className="hamburger">
+                    <button 
+                        className="navbar-brand col-sm-3 col-md-2 mx-2 my-4"
+                    >
+                        <MDBIcon icon="bars" size="1x" className="text-blue" />
+                    </button>
+                </div>
+
+                {/* Sidebar List Section */}
+                <div id="sidebar-list" className="sidebar-list">
+                    {lists.map(list => (
+                    <MenuItem 
+                        key={list.id} 
+                        {...list}
+                        activeList={(list.id === activeList) ? 'active' : ''}
+                        changeActive={changeActive}
+                        removeList={removeList}
+                        tasks={tasks}
+                    />
+                    ))}
+                </div>
+            
+            </MDBNav>
         </div>
-    </MDBCol>
-  )
+
+        {/* New List Section */}
+        <div className="new-list d-flex flex-row justify-content-start">
+            <button className="btn btn-link"><MDBIcon icon="plus" className="blue-text" /> New List</button>
+        </div>
+
+    </MDBCol> // Sidebar End
+)
 
 export default Sidebar;
